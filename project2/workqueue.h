fcdfcb3f475bac6b5873334a437e8cca1a3bb2d1
Jacob Sorber <jsorber@clemson.edu>
2026-09-24 10:56:16 -0400

    tweak

--- project2/workqueue.h
+++ project2/workqueue.h
@@ -12,7 +12,7 @@ typedef void *(*wq_job_fn)(void *arg);
 
 wq_t *wq_create(size_t num_workers, size_t queue_capacity);
 
-// blocking submit - waits until there is room on the queue, and then submis the task.
+// blocking submit - waits until there is room on the queue, and then submits the task.
 // returns the submitted task's id ()
 wq_job_id_t wq_submit(wq_t *q, wq_job_fn fn, void *arg);
 
